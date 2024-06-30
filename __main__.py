8cd6728df96ca789705c52ab5ec1bb8f0965d14b
Asaduzzaman Sohel <asadlive.sohel@gmail.com>
2024-07-01 02:42:10 +0600

    pulumi up

--- __main__.py
+++ __main__.py
@@ -7,9 +7,16 @@ import pulumi_aws as aws
 
 
 
-vpc = aws.ec2.Vpc("My-VPC-Asad1-test",
-    cidr_block="10.1.0.0/16",
+vpc = aws.ec2.Vpc("my-vpc-Asad",
+    cidr_block="10.10.0.0/16",
     enable_dns_hostnames=True,
     enable_dns_support=True
     )
+
+public_subnet = aws.ec2.Subnet("public-subnet-asad",
+                               vpc_id=vpc.id,
+                               cidr_block="10.10.1.0/24",
+                               map_public_ip_on_launch=True,
+                               availability_zone="ap-southeast-1a"
+                               )
 
